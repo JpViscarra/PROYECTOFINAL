52f5b90b920db3b27a226e89abe266589cf5b25b
JpViscarra <42975169+JpViscarra@users.noreply.github.com>
2018-09-17 21:57:01 -0400

    subida de la clase tipomueble

---
+++ MAKITA/src/proyecto/TipoMueble.java
@@ -0,0 +1,10 @@
+package proyecto;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class TipoMueble {
+	   private int IdTipoMueble;
+       private String Nombre;
+       private String Descripcion;
+       private LeerConsola leerConsola;
