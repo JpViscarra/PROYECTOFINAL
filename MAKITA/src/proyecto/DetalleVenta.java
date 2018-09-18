77618be934ecb4ebf682d02e2321c92aad6c18d0
JpViscarra <42975169+JpViscarra@users.noreply.github.com>
2018-09-18 02:32:16 -0400

    Add files via upload

--- MAKITA/src/proyecto/DetalleVenta.java
+++ MAKITA/src/proyecto/DetalleVenta.java
@@ -20,3 +20,24 @@ public class DetalleVenta {
      PrecioVenta =0;
      leerConsola= new LeerConsola();
 	}
+
+	public DetalleVenta(int IdDetalleVenta, int IdVenta, int IdMueble, int Cantidad , int PrecioVenta  )
+ {
+     this.IdDetalleVenta=IdDetalleVenta;
+     this.IdVenta=IdVenta;
+     this.IdMueble=IdMueble;
+     this.Cantidad=Cantidad;
+     this.PrecioVenta =PrecioVenta ;
+ }
+
+	public DetalleVenta(int codigo, OperacionesSQL operacionesSQL) throws SQLException
+	{
+		leerConsola = new LeerConsola();
+
+		
+
+		String query = "SELECT *  ";
+		query +=       "FROM   DetalleVenta ";
+		query +=       "WHERE  IdDetalleVenta = " + codigo;
+
+		ResultSet rs = operacionesSQL.selectSQL(query);
